e81413caeee0881a8261c9651b50c4279d0b76de
Miguel Matul <miguemat4@gmail.com>
2020-05-06 11:18:13 -0600

    Problema de comunicación entre hilos consumidores

--- Manotazo/src/main/frmMain.java
+++ Manotazo/src/main/frmMain.java
@@ -5,11 +5,18 @@
  */
 package main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
 /**
  *
  * @author migu_
  */
 public class frmMain extends javax.swing.JFrame {
+    
+    private final Productor generador = new Productor();
+    private int numero = 0;
 
     /**
      * Creates new form frmMain
@@ -17,6 +24,62 @@ public class frmMain extends javax.swing.JFrame {
     public frmMain() {
         initComponents();
         this.setLocationRelativeTo(null); // para centrar el frame
+        generador.start();
+        Consumidor jugador1, jugador2;
+        jugador1 = new Consumidor();
+        jugador1.estado = lblEstado1;
+        jugador1.contador = lblContador1;
+        jugador2 = new Consumidor();
+        jugador2.estado = lblEstado2;
+        jugador2.contador = lblContador2;
+        jugador1.start();
+        jugador2.start();
+    }
+    
+    private class Productor extends Thread{
+        @Override
+        public void run(){
+            while(true){
+                while(numero!=0)
+                    System.out.println("Esperando...");
+                try {
+                    Thread.sleep(2000); // 2 segundos para generar un número
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                numero = 1;
+                lblNumero.setText(String.valueOf(numero));
+                System.out.println("Acabo de producir: " + numero);
+            }
+        }
+    }
+    
+    private class Consumidor extends Thread{
+        private int limite=0, numeroAtrapado=0;
+        private JLabel estado, contador;
+        
+        @Override
+        public void run(){
+            while(true){
+                while(numero==0)
+                    System.out.println("Esperando...");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                numeroAtrapado = numero;
+                numero = 0;
+                lblNumero.setText("null");
+                aumentar();
+            }
+        }
+        
+        private void aumentar(){
+            limite+= numeroAtrapado;
+            contador.setText(String.valueOf(limite));
+            numeroAtrapado = 0;
+        }
     }
 
     /**
@@ -29,26 +92,106 @@ public class frmMain extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblNumero = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        lblContador1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        lblContador2 = new javax.swing.JLabel();
+        lblEstado1 = new javax.swing.JLabel();
+        lblEstado2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Evaluación final - Sistemas Operativos");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setText("Generador:");
+
+        lblNumero.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lblNumero.setText("null");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setText("Jugador 1");
+
+        lblContador1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblContador1.setText("0");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel4.setText("Jugador 2");
+
+        lblContador2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblContador2.setText("0");
+
+        lblEstado1.setText("Jugando...");
+
+        lblEstado2.setText("Jugando...");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(203, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(52, 52, 52)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(83, 83, 83)
+                        .addComponent(lblContador1)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(59, 59, 59))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblContador2)
+                        .addGap(93, 93, 93))))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(41, 41, 41)
+                                .addComponent(lblNumero)))))
+                .addContainerGap(125, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(66, 66, 66)
+                .addComponent(lblEstado1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblEstado2)
+                .addGap(72, 72, 72))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(275, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblNumero)
+                .addGap(45, 45, 45)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblContador1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblContador2)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblEstado1)
+                    .addComponent(lblEstado2))
+                .addGap(39, 39, 39))
         );
 
         pack();
@@ -89,5 +232,13 @@ public class frmMain extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblContador1;
+    private javax.swing.JLabel lblContador2;
+    private javax.swing.JLabel lblEstado1;
+    private javax.swing.JLabel lblEstado2;
+    private javax.swing.JLabel lblNumero;
     // End of variables declaration//GEN-END:variables
 }
